69fb55db72c616461fd682384a2f523b71fbe46f
Niels Delafontaine <pa76wlu@eduvaud.ch>
2024-11-25 12:07:08 +0100

    end of evaluation

--- src/main.py
+++ src/main.py
@@ -7,18 +7,19 @@ def main():
     result = calculate(operand1, operator, operand2)
     display_result(operand1, operator, operand2, result)
 
+
 def ask_user_input():
     # Get first operand from the user
     global operand1
-    operand1 = float(input("Enter the first operand: "))
+    operand1 = ask_user_float_input("Enter the first operand: ")
 
     global operator
     # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
+    operator = input("Enter an operator (+, -, *, /, ^): ")
 
     global operand2
     # Get second operand from the user
-    operand2 = float(input("Enter the second operand: "))
+    operand2 = ask_user_float_input("Enter the second operand: ")
 
 def calculate(ope1, oper, ope2):
     # Perform the operation based on the operator
@@ -26,7 +27,7 @@ def calculate(ope1, oper, ope2):
         case '+':
             res = ope1 + ope2
         case '-':
-            res = ope2 - ope2
+            res = ope1 - ope2
         case '*':
             res = ope1 * ope2
         case '/':
@@ -34,6 +35,8 @@ def calculate(ope1, oper, ope2):
                 print("Error: Division by zero is undefined.")
                 return
             res = ope1 / ope2
+        case '^':
+            res = fonctionpuissance(ope1,oper, ope2)
         case _:
             print("Invalid operator.")
             return
@@ -41,6 +44,24 @@ def calculate(ope1, oper, ope2):
 
 def display_result(op1, ope, ope2, res):
     print(str(op1) + " " + ope + " " + str(ope2) + " = " + str(res))
+
+#fonction float input
+def ask_user_float_input(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+# Fonction puissance
+def fonctionpuissance(ope1,oper, ope2):
+    puiss = 1
+    for _ in range(int(abs(ope2))):  # Utilisation de la valeur absolue pour gérer les puissances négatives
+        puiss *= ope1
+
+    if ope2 < 0:  # Gestion des puissances négatives
+        return 1 / puiss
+    return puiss
 
 # Call the main function to run the program
 main()
